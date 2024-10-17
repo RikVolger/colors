1cdb1f7edad49a277dc423a4037e71686202fb58
Rik Volger <rvolger@gmail.com>
2024-10-17 09:28:16 +0200

    Define colormaps

---
+++ colors.py
@@ -0,0 +1,76 @@
+import matplotlib
+
+# Blue sequential
+blues9 = [
+    [247, 251, 255],
+    [222, 235, 247],
+    [198, 219, 239],
+    [158, 202, 225],
+    [107, 174, 214],
+    [66, 146, 198],
+    [33, 113, 181],
+    [8, 81, 156],
+    [8, 48, 107]
+]
+
+blues9_map = matplotlib.colors.ListedColormap(blues9, name='blues9')
+
+# Green sequential
+greens9 = [
+    [247, 252, 245],
+    [229, 245, 224],
+    [199, 233, 192],
+    [161, 217, 155],
+    [116, 196, 118],
+    [65, 171, 93],
+    [35, 139, 69],
+    [0, 109, 44],
+    [0, 68, 27]
+]
+
+greens9_map = matplotlib.colors.ListedColormap(greens9, name='greens9')
+
+# Orange sequential
+oranges9 = [
+    [255, 245, 235],
+    [254, 230, 206],
+    [253, 208, 162],
+    [253, 174, 107],
+    [253, 141, 60],
+    [241, 105, 19],
+    [217, 72, 1],
+    [166, 54, 3],
+    [127, 39, 4]
+]
+
+oranges9_map = matplotlib.colors.ListedColormap(oranges9, name='oranges9')
+
+# Purple sequential
+purples9 = [
+    [252, 251, 253],
+    [239, 237, 245],
+    [218, 218, 235],
+    [188, 189, 220],
+    [158, 154, 200],
+    [128, 125, 186],
+    [106, 81, 163],
+    [84, 39, 143],
+    [63, 0, 125]
+]
+
+purples9_map = matplotlib.colors.ListedColormap(purples9, name='purples9')
+
+# Reds sequential
+reds9 = [
+    [255, 245, 240],
+    [254, 224, 210],
+    [252, 187, 161],
+    [252, 146, 114],
+    [251, 106, 74],
+    [239, 59, 44],
+    [203, 24, 29],
+    [165, 15, 21],
+    [103, 0, 13]
+]
+
+reds9_map = matplotlib.colors.ListedColormap(reds9, name='reds9')
